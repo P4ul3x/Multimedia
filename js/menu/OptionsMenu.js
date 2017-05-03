9336e8ed7ca7d64906b090769c72fcb6509bbcda
Roman <next-r@hotmail.com>
2017-05-03 16:34:32 +0100

    volume e back

--- js/menu/OptionsMenu.js
+++ js/menu/OptionsMenu.js
@@ -1,12 +1,28 @@
 "use strict";
 
-// @Version: 0.1.0
+// @Version: 0.1.1
 
 class OptionsMenu extends Menu {
 
 	constructor(){
 
 		super("OptionsMenu");
+		this.backButton = document.getElementById("OptionsMenuBackButton");
+		this.backButton.addEventListener("click",this.backButtonClickHandler);
+
+		this.volumeControl = document.getElementById("VolumeControl");
+		this.volumeControl.addEventListener("change", this.setVolume);
+		this.volumeControl.addEventListener("input", this.setVolume); // tive que adicionar 2 listeners para compatibilidade
+	}
+
+	backButtonClickHandler(){
+		window.dispatchEvent(new Event("showMainMenu"));
+	}
+
+	setVolume() {
+		var music = document.getElementById("music");
+		music.volume = this.value;
+		console.log(this.value);
 	}
 
 	static test(){
