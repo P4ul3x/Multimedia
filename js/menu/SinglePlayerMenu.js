8fa6cea99abd4efcdae8a21f0fc16cea2d8d6912
P4ul3x <p4ul3x97@gmail.com>
2017-05-05 12:02:19 +0100

    single player menu changes

--- js/menu/SinglePlayerMenu.js
+++ js/menu/SinglePlayerMenu.js
@@ -7,7 +7,10 @@ class SinglePlayerMenu extends Menu {
 	constructor(){
 		super("SinglePlayerMenu");
 		this.levelButton;
+		this.availableLevels = [];
 		this.characterButton;
+		this.availableCharacters = [1];
+		this.slideshow;
 		this.backButton;
 		this.startButton;
 		this.selectedLevel = -1;
@@ -21,36 +24,70 @@ class SinglePlayerMenu extends Menu {
 			this.characterButton[i].addEventListener("click", this.characterButtonClickHandler.bind(this));
 		}
 
+		this.slideshow = document.getElementById("slideshow");
 		this.backButton = document.getElementById("backButtonSinglePlayer");
-		this.backButton.addEventListener("click", this.backButtonClickHandler.bind(this));
-		
 		this.startButton = document.getElementById("startButtonSinglePlayer");
 
+		this.backButton.addEventListener("click", this.backButtonClickHandler.bind(this));
+		
 	}
+
+	/*
+		character Button Click Handler
+		-set new selected level
+		-change slideshow
+	*/
 
 	levelButtonClickHandler(ev){
 		ev.stopPropagation();
+		var selected;
+
+		//selected is the number of the button
+		selected = parseInt(ev.target.id.charAt(ev.target.id.length-1));
 		
-		if(this.selectedLevel>0)
+		if(this.selectedCharacter>0)
 			this.levelButton[this.selectedLevel-1].style.borderStyle = "outset";
 		
-		this.selectedLevel = ev.target.id.charAt(ev.target.id.length-1);
-		
-		ev.target.style.borderStyle = "inset";
+		if(this.availableLevels.includes(selected)){
+			this.selectedLevel = selected;
+			ev.target.style.borderStyle = "inset";
+		}
+		else{
+			this.selectedCharacter = -1;
+		}	
 		
 		console.log(this.selectedLevel);
 	}
 
+	/*
+		character Button Click Handler
+		-set new selected character
+		-change slideshow
+	*/
+
 	characterButtonClickHandler(ev){
 		ev.stopPropagation();
+		var selected;
+
+		//selected is the number of the button
+		selected = parseInt(ev.target.id.charAt(ev.target.id.length-1));
 		
 		if(this.selectedCharacter>0)
 			this.characterButton[this.selectedCharacter-1].style.borderStyle = "outset";
-		this.selectedCharacter = ev.target.id.charAt(ev.target.id.length-1);
 		
-		ev.target.style.borderStyle = "inset";
-		
-		console.log(this.selectedCharacter);	
+		if(this.availableCharacters.includes(selected)){
+			this.selectedCharacter = selected;
+			ev.target.style.borderStyle = "inset";
+			this.slideshow.style.backgroundImage = "url(../resources/menu/SinglePlayerMenu/character"+selected+".png)";
+			this.slideshow.style.animationName = "animationImage";
+			this.slideshow.style.backgroundSize = "1560px 390px";
+		}
+		else{
+			this.selectedCharacter = -1;
+			this.slideshow.style.backgroundImage = "url(../resources/menu/SinglePlayerMenu/notAvailable.png)";
+			this.slideshow.style.animationName = "none";
+			this.slideshow.style.backgroundSize = "390px 390px";
+		}	
 	}
 
 	/*
