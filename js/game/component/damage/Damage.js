c4efda48911f74394fabfe4a68c8bb27e01b0466
jpfbatista <jpfbatista@users.noreply.github.com>
2017-05-06 23:44:13 +0100

    Add files via upload

--- js/game/component/damage/Damage.js
+++ js/game/component/damage/Damage.js
@@ -9,6 +9,23 @@ class Damage extends Component {
 		this.value = value;
 	}
 
+	add(value){
+
+		if(typeof value !== "number") throw "Damage: add(): InvalidArgumentsException";
+
+		this.value += value;
+	}
+
+	subtract(value){
+
+		if(typeof value !== "number") throw "Damage: subtract(): InvalidArgumentsException";
+
+		this.value = Math.max(0,this.value-value);
+	}
+
+	/*
+		@OVERRIDE
+	*/
 	get value(){
 
 		return this.value;
