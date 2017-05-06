ecdf6f4cd8e5ff1e295ca44d650f795ae3b003fb
P4ul3x <p4ul3x97@gmail.com>
2017-05-06 15:50:35 +0100

    health js update

--- js/game/component/health/Health.js
+++ js/game/component/health/Health.js
@@ -20,10 +20,16 @@ class Health extends Component {
 	update(){
 
 		let damage = this.entity.getComponent("Damage");
-		if(damage) this.currentHealth = Math.max(0,this.currentHealth-damage.value);
+		if(damage) {
+			this.currentHealth = Math.max(0,this.currentHealth-damage.value);
+			this.entity.removeComponent("Damage");
+		}
 
 		let heal = this.entity.getComponent("Heal");
-		if(heal) this.currentHealth = Math.max(this.startingHealth,this.currentHealth+heal.value);
+		if(heal) {
+			this.currentHealth = Math.max(this.startingHealth,this.currentHealth+heal.value);
+			this.entity.removeComponent("Heal");
+		}
 
 		let revive = this.entity.getComponent("Revive");
 		if(revive) this.currentHealth = this.startingHealth;
