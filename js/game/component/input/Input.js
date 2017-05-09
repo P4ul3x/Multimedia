661fec7e2281b07c275bb9d33ac350d7361b93c5
jpfbatista <jpfbatista@users.noreply.github.com>
2017-05-09 16:43:48 +0100

    Add files via upload

---
+++ js/game/component/input/Input.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Input extends Component {
+
+	constructor(entity,framesToLive=Number.POSITIVE_INFINITY){
+
+		super(entity,framesToLive);
+	}
+}
+
---
+++ js/game/component/input/VelocityInput.js
@@ -0,0 +1,18 @@
+"use strict";
+
+class VelocityInput extends Input {
+
+	constructor(entity,framesToLive=Number.POSITIVE_INFINITY){
+
+		super(entity,framesToLive);
+	}
+
+	// @OVERRIDE
+	update(){
+
+		super.update();
+
+		// TODO
+	}
+}
+
