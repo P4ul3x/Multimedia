752a818df90c875df19871ca3c175f8a9d265dbf
jpfbatista <jpfbatista@users.noreply.github.com>
2017-05-09 16:07:14 +0100

    update implemented

--- js/game/component/draw/Draw.js
+++ js/game/component/draw/Draw.js
@@ -12,7 +12,25 @@ class Draw extends Component {
 
 		super.update();
 
-		// TODO
+		let representation = this.entity.getComponent("Representation");
+		if(!representation) return;
+
+		let visible = this.entity.getComponent("Visible");
+		if(!visible) return;
+
+		let position = this.entity.getComponent("Position");
+		if(!position) return;
+
+		let game = this.entity.game;
+		if(!game) return;
+
+		let context = game.context;
+		if(!context) return;
+
+		let imageData = representation.imageData;
+		if(!imageData) return;
+
+		context.putImageData(imageData,position.X,position.Y,imageData.width,imageData.height);
 	}
 }
 
