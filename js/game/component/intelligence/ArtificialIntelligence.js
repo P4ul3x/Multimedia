6a00bddc289653a777995c9cf1bd0e6281321015
jpfbatista <jpfbatista@users.noreply.github.com>
2017-05-09 18:05:39 +0100

    Add files via upload

---
+++ js/game/component/intelligence/ArtificialIntelligence.js
@@ -0,0 +1,18 @@
+"use strict";
+
+class ArtificialIntelligence extends Intelligence {
+
+	constructor(entity,framesToLive=Number.POSITIVE_INFINITY){
+
+		super(entity,framesToLive);
+	}
+
+	// @OVERRIDE
+	update(){
+
+		super.update();
+
+		// TODO
+	}
+}
+
---
+++ js/game/component/intelligence/Intelligence.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Intelligence extends Component {
+
+	constructor(entity,framesToLive=Number.POSITIVE_INFINITY){
+
+		super(entity,framesToLive);
+	}
+}
+
