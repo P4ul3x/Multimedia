b35a86093493f368717f8a126c556099031ba92b
P4ul3x <p4ul3x97@gmail.com>
2017-05-12 06:30:28 +0100

    new events, attack keys

--- js/global.js
+++ js/global.js
@@ -7,14 +7,20 @@
 window.GLOBAL = {
 	OBJECTS:{},
 	MOVEMENT_KEYS:{
-		UP:"ArrowUp",
-		RIGHT:"ArrowRight",
-		DOWN:"ArrowDown",
-		LEFT:"ArrowLeft"
+		UP:		"ArrowUp",
+		RIGHT: 	"ArrowRight",
+		DOWN: 	"ArrowDown",
+		LEFT: 	"ArrowLeft"
+	},
+	ATTACK_KEYS:{
+		BASIC: 	  "q",
+		THROW: 	  "w",
+		SPECIAL:  "e",
+		ULTIMATE: "r",
 	},
 	KEYS:{
-		ENTER:"Enter",
-		BACK:"b"
+		ENTER: 	"Enter",
+		BACK: 	"b"
 	}
 };
 
@@ -49,22 +55,36 @@ function nextIdentification(objectIdentification){
 }
 
 function keyDownHandler(keyEvent){
-	let key = keyEvent.key;
-	if(key == window.GLOBAL.MOVEMENT_KEYS.ENTER) window.dispatchEvent(new Event("enterKeyDown"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.BACK) window.dispatchEvent(new Event("backKeyDown"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.UP) window.dispatchEvent(new Event("upKeyDown"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.RIGHT) window.dispatchEvent(new Event("rightKeyDown"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.DOWN) window.dispatchEvent(new Event("downKeyDown"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.LEFT) window.dispatchEvent(new Event("leftKeyDown"));
+	let key = keyEvent.key; 
+	if(key == window.GLOBAL.KEYS.ENTER) window.dispatchEvent(new Event("enterKeyDown"));
+	else if(key == window.GLOBAL.KEYS.BACK) window.dispatchEvent(new Event("backKeyDown"));
+	
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.UP) 		window.dispatchEvent(new Event("upKeyDown"));
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.RIGHT)	window.dispatchEvent(new Event("rightKeyDown"));
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.DOWN) 	window.dispatchEvent(new Event("downKeyDown"));
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.LEFT) 	window.dispatchEvent(new Event("leftKeyDown"));
+	
+	else if(key == window.GLOBAL.ATTACK_KEYS.BASIC) 	window.dispatchEvent(new Event("basicKeyDown"));
+	else if(key == window.GLOBAL.ATTACK_KEYS.THROW) 	window.dispatchEvent(new Event("throwKeyDown"));
+	else if(key == window.GLOBAL.ATTACK_KEYS.SPECIAL) 	window.dispatchEvent(new Event("specialKeyDown"));
+	else if(key == window.GLOBAL.ATTACK_KEYS.ULTIMATE) 	window.dispatchEvent(new Event("ultimateKeyDown"));
+
 }
 
 function keyUpHandler(keyEvent){
 	let key = keyEvent.key;
 	if(key == window.GLOBAL.MOVEMENT_KEYS.ENTER) window.dispatchEvent(new Event("enterKeyUp"));
 	if(key == window.GLOBAL.MOVEMENT_KEYS.BACK) window.dispatchEvent(new Event("backKeyUp"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.UP) window.dispatchEvent(new Event("upKeyUp"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.RIGHT) window.dispatchEvent(new Event("rightKeyUp"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.DOWN) window.dispatchEvent(new Event("downKeyUp"));
-	else if(key == window.GLOBAL.MOVEMENT_KEYS.LEFT) window.dispatchEvent(new Event("leftKeyUp"));
+	
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.UP) 		window.dispatchEvent(new Event("upKeyUp"));
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.RIGHT) 	window.dispatchEvent(new Event("rightKeyUp"));
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.DOWN) 	window.dispatchEvent(new Event("downKeyUp"));
+	else if(key == window.GLOBAL.MOVEMENT_KEYS.LEFT)	 window.dispatchEvent(new Event("leftKeyUp"));
+
+	else if(key == window.GLOBAL.ATTACK_KEYS.BASIC) 	window.dispatchEvent(new Event("basicKeyUp"));
+	else if(key == window.GLOBAL.ATTACK_KEYS.THROW) 	window.dispatchEvent(new Event("throwKeyUp"));
+	else if(key == window.GLOBAL.ATTACK_KEYS.SPECIAL) 	window.dispatchEvent(new Event("specialKeyUp"));
+	else if(key == window.GLOBAL.ATTACK_KEYS.ULTIMATE) 	window.dispatchEvent(new Event("ultimateKeyUp"));
+
 }
 
