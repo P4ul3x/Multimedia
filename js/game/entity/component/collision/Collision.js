fadb20ef18e3d032184532eb3e3441e8e4a5d015
jpfbatista <jpfbatista@users.noreply.github.com>
2017-05-13 12:27:53 +0100

    Add files via upload

--- js/game/entity/component/collision/Collision.js
+++ js/game/entity/component/collision/Collision.js
@@ -11,7 +11,65 @@ class Collision extends Component {
 
 		super.update();
 
-		// TODO
+		let thisLiving = this.Living;
+
+		let thisPosition = thisLiving.getComponent("Position");
+
+		if(!thisPosition) return;
+
+		let thisAppearance = thisLiving.getComponent("Appearance");
+
+		if(!thisAppearance) return;
+
+		let thisImageData = thisAppearance.ImageData;
+
+		let livings = this.Game.Livings;
+
+		for(let i=0;i<livings.length;i++){
+
+			let otherLiving = livings[i];
+
+			if(thisLiving === otherLiving) continue;
+
+			if(!this.collidable(otherLiving)) continue;
+
+			let otherPosition = otherLiving.getComponent("Position");
+
+			if(!otherLiving) continue;
+
+			let otherAppearance = otherLiving.getComponent("Appearance");
+
+			if(!otherAppearance) continue;
+
+			let otherImageData = otherAppearance.ImageData;
+
+			if(this.boundingBoxCollide(thisPosition,thisImageData,otherPosition,otherImageData)
+			&& this.pixelCollide(thisPosition,thisImageData,otherPosition,otherImageData)
+			) this.collide();
+		}
+	}
+
+	collidable(living){
+
+		return living.getComponent("Collidable") ? true : false;
+	}
+
+	collide(){
+
+		console.log("Collision!");
+	}
+
+	boundingBoxCollide(position1,imageData1,position2,imageData2){
+
+		return position1.X < position2.X + imageData2.width
+			&& position1.X + imageData1.width > position2.X
+			&& position1.Y < position2.Y + imageData2.height
+			&& position1.Y + imageData1.height > position2.Y;
+	}
+
+	pixelCollide(position1,imageData1,position2,imageData2){
+
+		return true;
 	}
 }
 
