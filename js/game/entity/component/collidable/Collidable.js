749dc951425acd06b2450109f74812298e9b5575
jpfbatista <jpfbatista@users.noreply.github.com>
2017-05-13 12:27:15 +0100

    Add files via upload

---
+++ js/game/entity/component/collidable/Collidable.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Collidable extends Component {
+
+	constructor(living,game,priority=0,framesToLive=Number.POSITIVE_INFINITY){
+
+		super(living,game,priority,framesToLive);
+	}
+}
+
